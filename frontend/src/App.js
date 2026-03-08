5631a91322f6ba3fb3bc7335fc9fdad98dfc5099
nada <nw2052005@gmail.com>
2026-03-08 17:01:45 +0200

    Complete backend and frontend auth module

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Flights from "./pages/Flights";
 import Bookings from "./pages/Bookings";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import VerifyEmail from "./pages/VerifyEmail";
 
 function App() {
 
@@ -13,10 +16,15 @@ function App() {
       <Navbar />
 
       <Routes>
+        <Route path="/" element={<Register />} />
 
-        <Route path="/" element={<Flights />} />
+        <Route path="/flights" element={<Flights />} />
 
         <Route path="/bookings" element={<Bookings />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/verify-email" element={<VerifyEmail />} />
 
       </Routes>
 
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,8 +3,11 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav style={{marginBottom:"20px"}}>
-      <Link to="/">Flights</Link> |{" "}
-      <Link to="/bookings">My Bookings</Link>
+      <Link to="/flights">Flights</Link> |{" "}
+      <Link to="/bookings">My Bookings</Link>|{" "}
+      <Link to="/">Register</Link> |{" "}
+      <Link to="/login">Login</Link> |{" "}
+      <Link to="/verify-email">Verify Email</Link>
     </nav>
   );
 }
